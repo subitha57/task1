b6af7c5bb9045621a436a73fda5d87444e8950bc
subitha satheesh <147406447+subitha57@users.noreply.github.com>
2023-11-21 16:33:47 +0530

    star

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -6,5 +6,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
-  @Input() title: string = '';
+ // @Input() title: string = '';
+
+ @Input() rating: number = 0;
+
+ getFullStars(): number[] {
+   const fullStars = Math.floor(this.rating);
+   return Array(fullStars).fill(1);
+ }
+
+ hasHalfStar(): boolean {
+   return this.rating % 1 !== 0;
+ }
 }
